1e9782e8a4b33a62103dd17cbe61bdcc477dfe71
Patrick2OL <63985270+Patrick2OL@users.noreply.github.com>
2022-08-21 20:35:25 -0500

    Login system
    Add login system with user and roles

--- src/main/java/com/pry20220103/backend/Pry20220103BackendApplication.java
+++ src/main/java/com/pry20220103/backend/Pry20220103BackendApplication.java
@@ -2,7 +2,9 @@ package com.pry20220103.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Pry20220103BackendApplication {
 
